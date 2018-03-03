c71a349a62aae7c7b39915e26b5f77f00077ca1a
sue <sueheelee@outlook.com>
2018-03-02 16:39:15 -0800

    Refactored Carosel and Index Components

--- client/src/components/Carosel.jsx
+++ client/src/components/Carosel.jsx
@@ -1,60 +1,26 @@
 import React from 'react';
-import $ from 'jquery';
-
-let dupe = require('../../../seeds/test/dataDupe.js');
 
 class Carosel extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      display: dupe,
-      total: dupe.length,
-      current: 0,
-    }
   }
-  onRightClick(e) {
-    console.log('e',e.target.value);
-    console.log('right clicked')
-    if (this.state.current < this.state.total) {
-      let inc = this.state.current++;
-      $(".sliderbox").animate({left:"-=500"},500);
-      $(".right").animate({left:"+=500"},500);
-      $(".left").animate({left:"+=500"},500);
-      this.state.current++;
-    }
-  }
-
-  onLeftClick(e) {
-    console.log('left clicked')
-    console.log(e.target.value)
-    if (this.state.current > 1) {
-      let dec = this.state.current--;
-      $(".sliderbox").animate({left:"+=500"},500);
-      $(".right").animate({left:"-=500"},500);
-      $(".left").animate({left:"-=500"},500);
-      this.state.current--
-    }
-  }
-
   render() {
     return (
       <div className = 'outtermost' >     
         <div className = 'slidebinder' >
           <div className = 'sliderbox'>
-            <div className = 'left' onClick = {this.onLeftClick.bind(this)}>
+            <div className = 'left' onClick = {this.props.left} >
               <img src = 'http://www.thorlux.co.uk/shared/media/arrow-left-white.png'
                 width = '30px' height = '35px'/>
             </div>
             <div className = 'slide'>
-              {this.state.display.map(pic => 
-                <img key src ={this.state.display[this.state.current].src} 
-                  height = '500px' width = '500px'
-                />
+              {this.props.originState.dupePhotos.map(pic => 
+                <img key src = {pic.src} height = '600px' width = '600px'/>
               )}
             </div>
-            <div className = 'right' onClick = {this.onRightClick.bind(this)}>
+            <div className = 'right' onClick = {this.props.right} >
                <img src ='http://www.thorlux.com/shared/media/arrow-right-white.png'
-                width = '30px' height = '35px'/>
+                width = '30px' height = '35px' />
             </div>
           </div>
         </div>
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Container from './components/Container.jsx';
 import Carosel from './components/Carosel.jsx';
 import css from './style.css';
-
+import $ from 'jquery';
 
 let dupe = require('../../seeds/test/dataDupe.js');
 
@@ -12,35 +12,58 @@ class PhotoModule extends React.Component {
     super(props)
     this.state = {
       carosel: false,
-      dupePhotos: dupe
+      dupePhotos: dupe,
+      total: dupe.length,
+      current: 0,
     }
   }
-  handleClick(e) {
-    console.log('clicked',e)
-    // if (this.state.carosel === true) {
-
-    // }
-    console.log('e target',e.target.value)
-    this.setState({
-      carosel: !this.state.carosel
-    });
-     
+  handleClick() {
+      this.setState({
+        carosel: !this.state.carosel
+      });
   }
+  onRightClick() {
+    if (this.state.current < this.state.total) {
+      let inc = this.state.current++;
+      $(".sliderbox").animate({left:"-=600"},400);
+      $(".right").animate({left:"+=600"},400);
+      $(".left").animate({left:"+=600"},400);
+      // this.state.current++;
+      this.setState({
+        current:inc
+      })
+    }
+  }
+
+  onLeftClick() {
+    if (this.state.current > 1) {
+      let dec = this.state.current--;
+      $(".sliderbox").animate({left:"+=600"},400);
+      $(".right").animate({left:"-=600"},400);
+      $(".left").animate({left:"-=600"},400);
+      // this.state.current--
+      this.setState({
+        current:dec
+      })
+    }
+  }
+  
   render() { 
-    let display;
-
-    if (this.state.carosel) {
-      display = <Carosel photos = {this.state.dupePhotos} click = {this.handleClick.bind(this)}/>
-    } else {
-      display = <Container photos = {this.state.dupePhotos}/>
-    }
-
     return (
-      <div className = 'photomod' onClick = {this.handleClick.bind(this)}>
-        <span className = 'photoheader'>{this.state.dupePhotos.length} Photos</span>
-        <span className = 'viewmore'>View more</span>
-        <hr/>
-        {display}          
+      <div>
+        <div className = 'photomod' onClick = {this.handleClick.bind(this)}>
+          <span className = 'photoheader'>{this.state.dupePhotos.length} Photos</span>
+          <span className = 'viewmore'>View more</span>
+          <hr/>
+          <Container photos = {this.state.dupePhotos}/>
+        </div>
+
+        <div className = 'toggle-carosel'>
+        {(this.state.carosel)? <Carosel photos = {this.state.dupePhotos} click = {this.handleClick.bind(this)} 
+          left = {this.onLeftClick.bind(this)} right = {this.onRightClick.bind(this)}
+          originState = {this.state}
+          /> : <div></div>}
+        </div>
       </div>
     )
   }
